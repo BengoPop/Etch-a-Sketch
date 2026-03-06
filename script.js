322ffd8ca73867298160d33bd85e9dda4c40a94d
BengoPop <retr0astr4@gmail.com>
2026-03-05 23:25:28 -0500

    created a divbox just for example

---
+++ script.js
@@ -0,0 +1,10 @@
+const squareLength = "50px"
+
+const container = document.querySelector(".container");
+
+const divBox = document.createElement("div");
+divBox.style.width = squareLength;
+divBox.style.height = squareLength;
+divBox.style.border = "2px solid black";
+
+container.append(divBox);
