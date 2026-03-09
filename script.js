7d2f1aaaff2439a0de338125b778e22e11740b2b
BengoPop <retr0astr4@gmail.com>
2026-03-09 14:21:02 -0400

    implemented reset grid button

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-/*
- *
- *Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
-
-    Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
-    Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-    Also check out prompts.
-    You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
-
- *
- */
 const gridWidth = 960;
 const container = document.querySelector(".container");
+const resetButton = document.querySelector("#resetGrid");
 container.style.width = `${gridWidth}px`;
 
 
@@ -30,14 +20,15 @@ function drawGrid(numSquarePerLength) {
     createBox(numSquarePerLength);
   }
 }
-
 function getUserSquarePerSide() {
+  container.replaceChildren();
   let num = prompt("Set number of squares per side of new grid.");
+  while (num > 100) {
+    num = prompt("Number of squares per side can not be larger than 100. Try again")
+  }
   drawGrid(num);
 
 }
-
-getUserSquarePerSide()
-getUserSquarePerSide()
-
+getUserSquarePerSide();
+resetButton.addEventListener("click", (e) => getUserSquarePerSide());
 
